7711608a52b66302bd5032bb5e14cfe28cef31d1
David Cooley <david@leonardo.local>
2016-07-11 08:24:32 +1000

    initial commit

---
+++ myWidgetHTML/R/myHTML.R
@@ -0,0 +1,58 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+myHTML <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'myHTML',
+    x,
+    width = width,
+    height = height,
+    package = 'myWidgetHTML'
+  )
+}
+
+#' @import htmltools
+myHTML_html <- function(id, class, style, ...){
+	myString <- "hello world"
+	tags$p(myString)
+}
+
+#' Shiny bindings for myHTML
+#'
+#' Output and render functions for using myHTML within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a myHTML
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name myHTML-shiny
+#'
+#' @export
+myHTMLOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'myHTML', width, height, package = 'myWidgetHTML')
+}
+
+#' @rdname myHTML-shiny
+#' @export
+renderMyHTML <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, myHTMLOutput, env, quoted = TRUE)
+}
+
